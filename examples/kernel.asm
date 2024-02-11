0b40835323eadbecd02d9a890243309dd870c13b
Blackneeeed <dodatkowekonto.marcelclay@gmail.com>
2024-02-11 16:27:52 +0100

    Example Kernel

---
+++ examples/kernel.asm
@@ -0,0 +1,38 @@
+[bits 16]
+[org 0x7e00]
+
+section .text
+
+start: jmp main
+
+PUT_STRING:
+	push ax
+	push bx
+	.Loop:
+	cmp [bx], byte 0
+	je .Ret
+	mov ah, 0x0e
+	mov al, [bx]
+	int 0x10
+	inc bx
+	jmp .Loop
+	.Ret:
+	pop ax
+	pop bx
+	ret
+
+main:
+
+    mov bx, WELCOME_MSG
+    call PUT_STRING
+
+    cli
+    hlt
+
+section .data
+
+WELCOME_MSG: db 'Welcome from kernel loaded by KRNLload!', 0x0A, 0x0D, 0
+
+section .bss
+
+times 2048-($-$$) db 0
